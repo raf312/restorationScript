6334a0997f4d6da1df4907e9e663a8d74239b848
Renato <70023883+raf312@users.noreply.github.com>
2022-09-16 00:08:44 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #:Date              : jul 28 2022
 #:Author            : raf312
 #:Version           : 2.0.1
-#:Description       : The script restores an SDA disk image taken on the SAMSUNG HD322HJ hard drive. Here the drive to be restored is SDA. If I didn't know which drive to restore I could get the disk ID and restore it. As I know my drive is the SDA I already pre-set to 'partimage restore /dev/sdaX' (where X is the partition number).
+#:Description       : The script restores an SDA disk image taken on the SAMSUNG HD322HJ hard drive. Here the drive to be restored is SDA.
 
 clear
 
